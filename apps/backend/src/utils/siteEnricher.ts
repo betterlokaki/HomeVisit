b2f8b28faaad0b5b778616aa462d9bfa4169728b
LoKaki1 <shahartheking22@gmail.com>
2025-12-07 16:12:12 +0200

    Removed unused

--- apps/backend/src/utils/siteEnricher.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Site Data Enrichment Utilities
- * Re-exports for backward compatibility
- */
-
-export { calculateIntersectionPercent as calcaulteIntersectionPrecent } from "./intersectionCalculator.ts";
-export { filterOverlaysByIntersection } from "./overlayFilter.ts";
-export { createProjectLink } from "./projectLinkGenerator.ts";
